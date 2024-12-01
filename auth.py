5ee27a1042113e80eb995cc05a0659d6c411abe7
Priyanshu Raman <92126782+cryptic-pr03@users.noreply.github.com>
2024-12-01 16:01:55 +0530

    configured db and implemented authentication

---
+++ auth.py
@@ -0,0 +1,35 @@
+# Hash Passwords Using Passlib
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+#Configure JWT Authentication
+import jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "JWT_SECRET"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 10
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.ExpiredSignatureError:
+        return None
+    except jwt.InvalidTokenError:
+        return None
+
--- main.py
+++ main.py
@@ -1,16 +1,110 @@
-# This is a sample Python script.
+import os
+from typing import Annotated 
+from fastapi import Depends, FastAPI, HTTPException, Query, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlmodel import SQLModel, Session, create_engine, select
+from loguru import logger as log
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from auth import create_access_token, hash_password, verify_access_token, verify_password
+from db_utils import check_db_connection
+import models       
+
+app = FastAPI()
+
+#----------------------Database connection-----------------------------
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL, echo=True)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+#using fastapi dependency
+def get_session():
+    with Session(engine) as db:
+        yield db
+        
+SessionDep = Annotated[Session, Depends(get_session)]
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    payload = verify_access_token(token)
+    if payload is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+        )
+    return payload  
+
+UserDep = Annotated[dict, Depends(get_current_user)]
+#----------------------ROUTES-----------------------------
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get("/")
+async def home():
+    return {"message": "Hello World"}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from pydantic import BaseModel
+class Credentials(BaseModel):
+    email: str
+    password: str
+    
+# --------- AUTH -----------------
+@app.post("/register/")
+def register(user: Credentials, db: SessionDep):
+    log.info("Registering ...", Credentials)
+    db_user = db.exec(select(models.User).where(models.User.email == user.email)).first()
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            message="email already registered",
+        )
+    hashed_password = hash_password(user.password)
+    new_user = models.User(name="admin", email=user.email, hashed_password=hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return {"message": "User created successfully"}
+
+@app.post("/login/")
+async def login(user:Credentials, db: SessionDep):
+    log.info("Logging in ...", Credentials)
+    db_user = db.exec(select(models.User).where(models.User.email == user.email)).first()
+    if not db_user or not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password",
+        )
+    access_token = create_access_token({"sub": db_user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
+  
+
+
+
+@app.get("/protected/")
+def protected_route(current_user: UserDep):
+    log.info("Current user...", current_user)
+    return {"message": f"Hello {current_user['sub']}, you are authenticated!"}
+
+@app.get("/users/") 
+async def read_users(
+    db: SessionDep, 
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[models.User]:
+    users = db.exec(select(models.User).offset(offset).limit(limit)).all()
+    return users
+    
+
+#----------------------START_UP-----------------------------
+@app.on_event("startup")
+def on_startup():
+    with Session(engine) as db:      
+        connection_info = check_db_connection(db)
+        log.info(connection_info)
+        if connection_info["flag"]:
+            log.success(connection_info["status"])
+        else:
+            log.info(connection_info["status"])
+            log.error(connection_info["error"])
+            exit()
+    
+    #create db and tables
+    SQLModel.metadata.create_all(engine)
